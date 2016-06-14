6cdd12229396f714c64d8197a997cf6b926db751
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-06-14 16:09:43 -0600

    basic crud working

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,12 @@
 // Update with your config settings.
 
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
+    development: {
+        client: 'pg',
+        connection: 'postgres://localhost/CRUD_DEMO' || process.env.DATABASE_URL
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL + '?ssl=true'
     }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
